6b44eb386cc9721fd599db8ce3a7c6b49e06a5cd
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-04-03 03:18:14 +0530

    ddddd

---
+++ src/sections/MemeMark.jsx
@@ -0,0 +1,184 @@
+import React from "react";
+import Marquee from "react-fast-marquee";
+
+function MemeMark() {
+  return (
+    <>
+      <section>
+        <div>
+          <Marquee>
+            <img
+              src="images/logo.png"
+              alt=""
+              className="h-[30px] w-auto px-2"
+            />
+
+            <h2 className="text-2xl text-center px-1 font-sushi-one ">
+              SUSHI CAT
+            </h2>
+
+            <img
+              src="images/susi.png"
+              alt=""
+              className="h-[30px] w-auto px-2"
+            />
+
+            <img
+              src="images/logo.png"
+              alt=""
+              className="h-[30px] w-auto px-2"
+            />
+
+            <h2 className="text-2xl text-center px-1 font-sushi-one ">
+              SUSHI CAT
+            </h2>
+
+            <img
+              src="images/susi.png"
+              alt=""
+              className="h-[30px] w-auto px-2"
+            />
+
+            <img
+              src="images/logo.png"
+              alt=""
+              className="h-[30px] w-auto px-2"
+            />
+
+            <h2 className="text-2xl text-center px-1 font-sushi-one ">
+              SUSHI CAT
+            </h2>
+
+            <img
+              src="images/susi.png"
+              alt=""
+              className="h-[30px] w-auto px-2"
+            />
+
+            <img
+              src="images/logo.png"
+              alt=""
+              className="h-[30px] w-auto px-2"
+            />
+
+            <h2 className="text-2xl text-center px-1 font-sushi-one ">
+              SUSHI CAT
+            </h2>
+
+            <img
+              src="images/susi.png"
+              alt=""
+              className="h-[30px] w-auto px-2"
+            />
+
+            <img
+              src="images/logo.png"
+              alt=""
+              className="h-[30px] w-auto px-2"
+            />
+
+            <h2 className="text-2xl text-center px-1 font-sushi-one ">
+              SUSHI CAT New ATH
+            </h2>
+
+            <img
+              src="images/susi.png"
+              alt=""
+              className="h-[30px] w-auto px-2"
+            />
+
+            <img
+              src="images/logo.png"
+              alt=""
+              className="h-[30px] w-auto px-2"
+            />
+
+            <h2 className="text-2xl text-center px-1 font-sushi-one ">
+              To The Moon
+            </h2>
+
+            <img
+              src="images/susi.png"
+              alt=""
+              className="h-[30px] w-auto px-2"
+            />
+
+<img
+              src="images/logo.png"
+              alt=""
+              className="h-[30px] w-auto px-2"
+            />
+
+            <h2 className="text-2xl text-center px-1 font-sushi-one ">
+              SUSHI CAT
+            </h2>
+
+            <img
+              src="images/susi.png"
+              alt=""
+              className="h-[30px] w-auto px-2"
+            />
+
+
+
+
+<img
+              src="images/logo.png"
+              alt=""
+              className="h-[30px] w-auto px-2"
+            />
+
+            <h2 className="text-2xl text-center px-1 font-sushi-one ">
+              SUSHI CAT
+            </h2>
+
+            <img
+              src="images/susi.png"
+              alt=""
+              className="h-[30px] w-auto px-2"
+            />
+
+
+
+
+<img
+              src="images/logo.png"
+              alt=""
+              className="h-[30px] w-auto px-2"
+            />
+
+            <h2 className="text-2xl text-center px-1 font-sushi-one ">
+              SUSHI CAT
+            </h2>
+
+            <img
+              src="images/susi.png"
+              alt=""
+              className="h-[30px] w-auto px-2"
+            />
+
+
+
+<img
+              src="images/logo.png"
+              alt=""
+              className="h-[30px] w-auto px-2"
+            />
+
+            <h2 className="text-2xl text-center px-1 font-sushi-one ">
+              SUSHI CAT
+            </h2>
+
+            <img
+              src="images/susi.png"
+              alt=""
+              className="h-[30px] w-auto px-2"
+            />
+          </Marquee>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default MemeMark;
